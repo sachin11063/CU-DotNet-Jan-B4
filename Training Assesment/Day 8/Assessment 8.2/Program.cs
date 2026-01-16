3cb64d4ab5571ca74e5686a17b521d1d3b866320
sachin11063 <22BCS11063@cuchd.in>
2026-01-16 10:43:50 +0530

    Day 8.2

---
+++ Training Assesment/Day 8/Assessment 8.2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Value: ");
+        string input = Console.ReadLine();
+        string[] parts = input.Split('#');
+
+        string transactionId = parts[0];
+        string accountHolder = parts[1];
+        string narration = parts[2];
+
+        //Normalize narration
+        narration = narration.Trim();
+
+        //Replace multiple spaces with single space
+        while (narration.Contains("  "))
+        {
+            narration = narration.Replace("  ", " ");
+        }
+
+        narration = narration.ToLower();
+
+        //Keyword identification
+        bool hasKeyword =
+            narration.Contains("deposit") ||
+            narration.Contains("withdrawal") ||
+            narration.Contains("transfer");
+
+        //Compare with standard narration
+        string standardNarration = "cash deposit successful";
+        bool isStandard = narration.Equals(standardNarration);
+
+        string category;
+
+        if (!hasKeyword)
+        {
+            category = "NON-FINANCIAL TRANSACTION";
+        }
+        else if (isStandard)
+        {
+            category = "STANDARD TRANSACTION";
+        }
+        else
+        {
+            category = "CUSTOM TRANSACTION";
+        }
+
+        //output
+        Console.WriteLine("Transaction ID : " + transactionId);
+        Console.WriteLine("Account Holder : " + accountHolder);
+        Console.WriteLine("Narration      : " + narration);
+        Console.WriteLine("Category       : " + category);
+    }
+}
