6f873fec2ca9da63af408849f15e2f71ef9911f9
sachin11063 <22BCS11063@cuchd.in>
2026-01-24 12:26:42 +0530

    Uploading Day 15

---
+++ Training Assesment/Day 15/5.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace HeightAddition
+{
+    class Height
+    {
+        public int Feet { get; set; }
+        public double Inches { get; set; }
+
+               public Height()
+        {
+            Feet = 0;
+            Inches = 0.0;
+        }
+
+    
+        public Height(int feet, double inches)
+        {
+            Feet = feet;
+            Inches = inches;
+        }
+
+        
+        public Height AddHeights(Height h2)
+        {
+            int totalFeet = this.Feet + h2.Feet;
+            double totalInches = this.Inches + h2.Inches;
+
+            if (totalInches >= 12)
+            {
+                totalFeet += (int)(totalInches / 12);
+                totalInches = totalInches % 12;
+            }
+
+            return new Height(totalFeet, totalInches);
+        }
+
+        public override string ToString()
+        {
+            return $"Height - {Feet} feet {Inches} inches";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Height person1 = new Height(5, 6.5);
+            Height person2 = new Height(5, 7.5);
+
+            Height totalHeight = person1.AddHeights(person2);
+
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+            Console.WriteLine(totalHeight);
+        }
+    }
+}
---
+++ Training Assesment/Day 15/5.2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace OrderProcessing
+{
+    class Order
+    {
+        private int _orderId;
+        private string _customerName;
+        private decimal _totalAmount;
+        private bool _discountApplied;
+
+        public Order()
+        {
+            _orderId = 0;
+            _customerName = "Unknown";
+            _totalAmount = 0;
+            _discountApplied = false;
+        }
+
+        public Order(int orderId, string customerName)
+        {
+            _orderId = orderId;
+            CustomerName = customerName; // validation via property
+            _totalAmount = 0;
+            _discountApplied = false;
+        }
+
+        public int OrderId
+        {
+            get { return _orderId; }
+        }
+
+        public string CustomerName
+        {
+            get { return _customerName; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _customerName = value;
+                }
+            }
+        }
+
+        public decimal TotalAmount
+        {
+            get { return _totalAmount; }
+        }
+
+        public void AddItem(decimal price)
+        {
+            if (price > 0)
+            {
+                _totalAmount += price;
+            }
+        }
+
+        public void ApplyDiscount(decimal percentage)
+        {
+            if (!_discountApplied && percentage >= 1 && percentage <= 30)
+            {
+                decimal discount = _totalAmount * (percentage / 100);
+                _totalAmount -= discount;
+                _discountApplied = true;
+            }
+        }
+
+        public string GetOrderSummary()
+        {
+            return $"Order Id: {OrderId}\n" +
+                   $"Customer: {CustomerName}\n" +
+                   $"Total Amount: {_totalAmount}\n" +
+                   $"Status: NEW";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order1 = new Order(101, "Rahul");
+            order1.AddItem(500);
+            order1.AddItem(300);
+            order1.ApplyDiscount(10);
+
+            Console.WriteLine(order1.GetOrderSummary());
+
+            Console.WriteLine();
+
+            Order order2 = new Order();
+            order2.CustomerName = "Sachin";
+            order2.AddItem(1000);
+
+            Console.WriteLine(order2.GetOrderSummary());
+        }
+    }
+}
