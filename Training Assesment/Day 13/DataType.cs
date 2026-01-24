546bff04188d8fb392af0a69348e94730b345b58
sachin11063 <22BCS11063@cuchd.in>
2026-01-24 12:07:12 +0530

    Some Changes

---
+++ Training Assesment/Day 13/DataType.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CSLearning
+{
+	internal class DataType
+    {	
+        static void PrintLine(int num = 40, char ch = '-')
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            PrintLine();
+            PrintLine(ch:'$');
+            PrintLine(60, '+');
+            PrintLine(ch: '@', num: 70);
+            PrintLine('$');
+            PrintLine(70);
+
+        }
+
+    }
+}
+
