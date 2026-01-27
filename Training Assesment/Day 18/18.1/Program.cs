d4aff8fb1d42c6f4c541fc66182b9ad116e7c5e6
sachin11063 <22BCS11063@cuchd.in>
2026-01-27 13:57:12 +0530

    Update day 18 UploadinD

---
+++ Training Assesment/Day 18/18.1/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Day_17;
+
+public class Loan
+{
+    public string LoanNumber { get; set; }
+    public string CustomerName { get; set; }
+    public decimal PrincipalAmount { get; set; }
+    public int TenureInYear { get; set; }
+
+    public Loan(string loanNumber, string customerName, decimal principalAmount, int tenureInYear)
+    {
+        LoanNumber = loanNumber;
+        CustomerName = customerName;
+        PrincipalAmount = principalAmount;
+        TenureInYear = tenureInYear;
+    }
+
+    public decimal CalculateEMI()
+    {
+        return PrincipalAmount + (PrincipalAmount * 0.10m);
+    }
+}
+
+
+public class HomeLoan : Loan
+{
+    public HomeLoan(string loanNumber, string customerName,
+                    decimal principalAmount, int tenureInYear)
+        : base(loanNumber, customerName, principalAmount, tenureInYear)
+    {
+    }
+    public decimal CalculateEMI()
+    {
+        return PrincipalAmount + (TenureInYear * (PrincipalAmount * 0.08m)) + (PrincipalAmount * 0.01m);
+    }
+}
+
+public class CarLoan : Loan
+{
+    public CarLoan(string loanNumber, string customerName,
+                   decimal principalAmount, int tenureInYear)
+        : base(loanNumber, customerName, principalAmount, tenureInYear)
+    {
+    }
+
+    public decimal CalculateEMI()
+    {
+        return PrincipalAmount + (TenureInYear * (PrincipalAmount * 0.09m)) + 15000;
+    }
+}
+
+
+internal class Demo
+{
+    public static void Main(string[] args)
+    {
+        Loan[] loans =
+        {
+            new HomeLoan("HL101", "Sachin", 1200000, 5),
+            new HomeLoan("HL102", "Amit", 1500000, 10),
+            new CarLoan("CL101", "Rahul", 500000, 6),
+            new CarLoan("CL102", "Neha", 600000, 7)
+        };
+
+        foreach (Loan loan in loans)
+        {
+            Console.WriteLine($"Loan No: {loan.LoanNumber}, Customer Name: {loan.CustomerName}, EMI: {loan.CalculateEMI()}");
+        }
+    }
+}
+
+
