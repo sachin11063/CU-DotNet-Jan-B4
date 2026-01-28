a74bd7ae061cd9d476fa18e77a4ba18ee8944b25
sachin11063 <22BCS11063@cuchd.in>
2026-01-28 14:44:30 +0530

    Uploading Day 19 Assignment

---
+++ Training Assesment/Day 19/19.1/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Day19;
+
+
+abstract class Vehicle
+{
+    public string ModelName { get; set; }
+    public abstract void Move();
+    public Vehicle(string ModelName)
+    {
+        this.ModelName = ModelName;
+    }
+    public virtual void GetFuelStatus()
+    {
+        Console.WriteLine("Fuel Level is Stable.");
+    }
+}
+
+class ElectricCar : Vehicle
+{
+    public ElectricCar(string ModelName) : base(ModelName)
+    {
+
+    }
+    public override void Move()
+    {
+        Console.WriteLine($"Your {ModelName} is gliding silently on battery power.");
+    }
+    public override void GetFuelStatus()
+    {
+        Console.WriteLine($"Your {ModelName} battery is at 80 %");
+    }
+}
+
+class HeavyTruck : Vehicle
+{
+    public HeavyTruck(string ModelName) : base(ModelName)
+    {
+
+    }
+    public override void Move()
+    {
+        Console.WriteLine($"Your {ModelName} is is hauling cargo with high-torque diesel power.");
+    }
+
+}
+
+class CargoPlane : Vehicle
+{
+    public CargoPlane(string ModelName) : base(ModelName)
+    {
+
+    }
+    public override void Move()
+    {
+        Console.WriteLine($"Your {ModelName} is ascending to 30,000 feet.");
+    }
+
+    public override void GetFuelStatus()
+    {
+        base.GetFuelStatus();
+        Console.WriteLine("Checking jet fuel reserves...");
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle[] vehicles =
+        {
+        new ElectricCar ( "Tesla" ),
+        new HeavyTruck ( "Volvo" ),
+        new CargoPlane ( "Boeing 747 Cargo" )
+        };
+
+        foreach (Vehicle vehicle in vehicles)
+        {
+            Console.WriteLine($"--- {vehicle.ModelName} ---");
+            vehicle.Move();
+            vehicle.GetFuelStatus();
+            Console.WriteLine();
+        }
+    }
+
+}
+
---
+++ Training Assesment/Day 19/19.2/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace Day19._2;
+
+
+abstract class UtilityBill
+{
+    public int ConsumerId { get; set; }
+    public string ConsumerName { get; set; }
+    public decimal UnitsConsumed { get; set; }
+    public decimal RatePerUnit { get; set; }
+
+    public abstract decimal CalculateBillAmount();
+
+    public virtual decimal CalculateTax(decimal billAmount)
+    {
+        return billAmount + (billAmount * 0.05m);
+    }
+
+
+    public void PrintBill()
+    {
+        decimal billAmount = CalculateBillAmount();
+        decimal tax = CalculateTax(billAmount);
+        decimal finalAmount = billAmount + tax;
+
+        Console.WriteLine($"Consumer ID   : {ConsumerId}");
+        Console.WriteLine($"Consumer Name : {ConsumerName}");
+        Console.WriteLine($"Units Used    : {UnitsConsumed}");
+        Console.WriteLine($"Bill Amount   : ₹{billAmount}");
+        Console.WriteLine($"Tax           : ₹{tax}");
+        Console.WriteLine($"Payable Amount: ₹{finalAmount}");
+    }
+
+
+}
+class ElectricityBill : UtilityBill
+{
+    public override decimal CalculateBillAmount()
+    {
+        decimal amount = UnitsConsumed * RatePerUnit;
+        if (UnitsConsumed > 300)
+        {
+            amount += amount * 0.10m;
+        }
+        return amount;
+    }
+}
+
+class WaterBill : UtilityBill
+{
+    public override decimal CalculateBillAmount()
+    {
+        return (UnitsConsumed * RatePerUnit);
+    }
+    public override decimal CalculateTax(decimal billAmount)
+    {
+        return (billAmount * 0.02m);
+    }
+}
+
+class GasBill : UtilityBill
+{
+    public override decimal CalculateBillAmount()
+    {
+        return (UnitsConsumed * RatePerUnit) + 150;
+    }
+
+    public override decimal CalculateTax(decimal billAmount)
+    {
+        return 0;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<UtilityBill> bills = new List<UtilityBill>
+        {
+            new ElectricityBill
+            {
+                ConsumerId = 101,
+                ConsumerName = "Sachin",
+                UnitsConsumed = 350,
+                RatePerUnit = 6.5m
+            },
+
+            new WaterBill
+            {
+                ConsumerId = 102,
+                ConsumerName = "Akash",
+                UnitsConsumed = 120,
+                RatePerUnit = 2.0m
+            },
+
+            new GasBill
+            {
+                ConsumerId = 103,
+                ConsumerName = "Rahul",
+                UnitsConsumed = 30,
+                RatePerUnit = 15m
+            }
+        };
+
+        foreach (var item in bills)
+        {
+            item.PrintBill();
+            Console.WriteLine();
+        }
+    }
+}
