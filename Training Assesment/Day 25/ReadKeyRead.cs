fd43e36b3d4c7cd7e293ab31543c8befb93a0ebb
sachin11063 <22BCS11063@cuchd.in>
2026-02-10 18:48:22 +0530

    file

---
+++ Training Assesment/Day 25/ReadKeyRead.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class SecureTerminal
+{
+    static void Main()
+    {
+        string pin = "";
+        ConsoleKeyInfo keyInfo;
+
+        Console.Write("Enter 4-digit PIN: ");
+
+        while (pin.Length < 4)
+        {
+            keyInfo = Console.ReadKey(true);
+
+            if (char.IsDigit(keyInfo.KeyChar))
+            {
+                pin += keyInfo.KeyChar;
+                Console.Write("*");
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("PIN Entered: " + pin);
+
+        Console.Write("Enter System Message: ");
+        string systemMessage = Console.ReadLine();
+
+        Console.WriteLine("System Message:");
+        Console.WriteLine(systemMessage);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
