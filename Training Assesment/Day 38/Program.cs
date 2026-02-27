4e3b7cdf94823b087927e4f819429b176e5946cc
sachin11063 <22BCS11063@cuchd.in>
2026-02-27 11:36:47 +0530

    Day 38

---
+++ Training Assesment/Day 38/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "abcdu";
+        string result = Transform(input);
+        Console.WriteLine(result);
+    }
+
+    static string Transform(string input)
+    {
+        string vowels = "aeiou";
+        StringBuilder output = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if (vowels.Contains(c))
+            {
+                int index = vowels.IndexOf(c);
+                char nextVowel = vowels[(index + 1) % vowels.Length];
+                output.Append(nextVowel);
+            }
+            else if (c >= 'a' && c <= 'z')
+            {
+                char nextChar = c == 'z' ? 'a' : (char)(c + 1);
+
+                while ("aeiou".Contains(nextChar))
+                {
+                    nextChar = nextChar == 'z' ? 'a' : (char)(nextChar + 1);
+                }
+
+                output.Append(nextChar);
+            }
+            else
+            {
+                output.Append(c);
+            }
+        }
+
+        return output.ToString();
+    }
+}
